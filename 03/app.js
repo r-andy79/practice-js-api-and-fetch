5623e756046fc482934564836093b0b734e30eaf
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2021-02-08 08:54:02 +0000

    Zadanie nr 3 zrobione

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,23 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+  const btnEl = document.querySelector('button');
+  btnEl.addEventListener('click', getData);
+}
+
+function getData() {
+  fetch('https://api.ipify.org')
+    .then((resp) => {
+      if (resp.ok) {
+        return resp.text();
+      }
+      return Promise.reject(resp);
+    })
+    .then((ip) => insertData(ip))
+    .catch((err) => console.error(err));
+}
+
+function insertData(data) {
+  const spanEl = document.querySelector('span');
+  spanEl.innerText = `${data}`;
 }
